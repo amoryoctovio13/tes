1edc422c3fa21c527b3791dbcec7ae704ff84103
amory octovio <amoryoctovio13@gmail.com>
2021-05-10 10:21:40 +0700

    tugas akhir

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+ 
+use Illuminate\Support\Facades\Route; 
+ 
+Route::get('/', 'AuthController@showFormLogin')->name('login');
+Route::get('login', 'AuthController@showFormLogin')->name('login');
+Route::post('login', 'AuthController@login');
+Route::get('register', 'AuthController@showFormRegister')->name('register');
+Route::post('register', 'AuthController@register');
+ 
+Route::group(['middleware' => 'auth'], function () {
+ 
+    Route::get('home', 'HomeController@index')->name('home');
+    Route::get('logout', 'AuthController@logout')->name('logout');
+ 
+});
